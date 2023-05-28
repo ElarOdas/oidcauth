038bc9053ce3174dbb4c7d5510b9734ca7b512f1
Elarodas <patrick.volpert@gmx.de>
2023-05-28 22:11:01 +0200

    Add log functionality

--- authenticator.go
+++ authenticator.go
@@ -10,50 +10,55 @@ import (
 	"github.com/lestrrat-go/jwx/v2/jwt"
 )
 
-func Authenticator(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token, _, err := FromContext(r.Context())
+func Authenticator(logf func(v ...any)) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		hfn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			token, _, err := FromContext(r.Context())
 
-		//TODO Error Codes according to https://www.rfc-editor.org/rfc/rfc6750#section-3.1
+			//TODO Error Codes according to https://www.rfc-editor.org/rfc/rfc6750#section-3.1
 
-		// Default
+			// Default
 
-		if err != nil {
-			var invalidRequest *offline.InvalidRequestError
-			var invalidToken *offline.InvalidTokenError
-			//  400 invalid request
-			if errors.As(err, &invalidRequest) {
-				w.Header().Add("WWW-Authenticate", fmt.Sprintf("Bearer %s", err.Error()))
+			if err != nil {
+				logf("failed token auth:", err)
+				var invalidRequest *offline.InvalidRequestError
+				var invalidToken *offline.InvalidTokenError
+				//  400 invalid request
+				if errors.As(err, &invalidRequest) {
+					w.Header().Add("WWW-Authenticate", fmt.Sprintf("Bearer %s", err.Error()))
+					http.Error(w, "", http.StatusBadRequest)
+
+					return
+				}
+				// 401 invalid token
+				if errors.As(err, &invalidToken) {
+					w.Header().Add("WWW-Authenticate", fmt.Sprintf("Bearer %s", err.Error()))
+					http.Error(w, "", http.StatusUnauthorized)
+					return
+				}
+				// if errors.As(err, &insufficientScope) {
+				// 	w.Header().Add("WWW-Authenticate", fmt.Sprintf("Bearer %s", err.Error()))
+				// 	http.Error(w, "", http.StatusForbidden)
+				// 	log.Error(errors.Unwrap(err))
+				// 	return
+				// }
+
+				// Default to 400
 				http.Error(w, "", http.StatusBadRequest)
+				return
 
+			}
+
+			if token == nil || jwt.Validate(token) != nil {
+				http.Error(w, "", http.StatusBadRequest)
 				return
 			}
-			// 401 invalid token
-			if errors.As(err, &invalidToken) {
-				w.Header().Add("WWW-Authenticate", fmt.Sprintf("Bearer %s", err.Error()))
-				http.Error(w, "", http.StatusUnauthorized)
-				return
-			}
-			// if errors.As(err, &insufficientScope) {
-			// 	w.Header().Add("WWW-Authenticate", fmt.Sprintf("Bearer %s", err.Error()))
-			// 	http.Error(w, "", http.StatusForbidden)
-			// 	log.Error(errors.Unwrap(err))
-			// 	return
-			// }
 
-			// Default to 400
-			http.Error(w, "", http.StatusBadRequest)
-			return
+			// Token is authenticated, pass it through
+			next.ServeHTTP(w, r)
 
-		}
+		})
+		return hfn
+	}
 
-		if token == nil || jwt.Validate(token) != nil {
-			http.Error(w, "", http.StatusBadRequest)
-			return
-		}
-
-		// Token is authenticated, pass it through
-		next.ServeHTTP(w, r)
-
-	})
 }
